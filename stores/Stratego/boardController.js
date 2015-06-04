ed89329bc4a9b2c9689d349251d728642ecc32ee
jacobww@gmail.com <jacobww@gmail.com>
2015-06-04 13:46:07 +0200

    Added game rules logic

--- stores/Stratego/boardController.js
+++ stores/Stratego/boardController.js
@@ -2,9 +2,9 @@ module.exports = {
     // Assume that from and to are pairs indicating a position (ex. {row :1, col:2})
     // Assumes that board is an immutable list of lists
     move: function(board, from, to) {
-    if ( moveIsPossible(board, from, to)) {
-        return doAttackAndUpdateBoard(from, to);
-    }
+        if ( moveIsPossible(board, from, to)) {
+            return doAttackAndUpdateBoard(from, to);
+        }
     },
     moveIsPossible : function(board, from, to) {
         // Determine if move is possible.
@@ -14,23 +14,55 @@ module.exports = {
         //  - all positions between from and two are of type plain
         //  - all positions between from and two are unoccupied by other checkers
         //  - player at from is different from player at to
+        var fromPosition = board.get(from.row).get(from.col);
+        var toPosition = board.get(to.row).get(to.col);
 
+        var thereIsACheckerInFromPosition = fromPosition.get("checker") != null;
+        var correspondRowOrColumn = from.row == to.row || from.col == to.col;
+        var pathIsTraversable = pathIsTraversable(board, from, to, from.row != to.row);
+        var playerIsDifferent = fromPosition.get("checker").player != toPosition.get("checker").player;
+        return thereIsACheckerInFromPosition && correspondRowOrColumn && pathIsTraversable && playerIsDifferent;
+    },
+    pathIsTraversable : function(board, from, to, isRowMove) {
+        var result = true;
+        if ( isRowMove) {
+            // Dont include the final row, this is the target position
+            for( var row = from.row; row <= to.row - 1 && result; row++) {
+                var position = board.getIn([row,from.column]);
+                result = result && position.get("field").traversable && position.get("checker")== null;
+            }
+        }
+        else {
+            // Dont include the final column, this is the target position
+            for( var column = from.column; column <= to.column - 1 && result; column++) {
+                var position = board.getIn([from.row,column]);
+                result = result && position.get("field").traversable && position.get("checker")== null;
+            }
+        }
+        return result;
     },
     doAttackAndUpdateBoard : function(board, from, to) {
         // Attack logic is as follows:
         // Positions are immutable records
         var attackingPosition = board.get(from.row).get(from.col);
         var defendingPosition = board.get(to.row).get(to.col);
-        if ( defendingPosition.get("checker") == null) { // Attacking an empty position is always a win
+        if ( attackerWins(attackingPosition,defendingPosition)) { // Attacking an empty position is always a win
             var newAttackingPosition = attackingPosition.set("checker",  null);
             var newDefendingPosition = defendingPosition.set("checker", attackingPosition.checker);
             // update the board with the new positions
             board = board.setIn([from.row,from.col], newAttackingPosition);
             return board.setIn([to.row,to.col], newDefendingPosition);
         }
-        if ( attackingPosition.checker.attackRank > defendingPosition.checker.defenceRank ||
-            (attackingPosition.checker.specialAttack && attackingPosition.checker.specialAttack(defendingPosition.checker))) {
-            // Attacker wins
+        else { // remove attacker from map
+            var newAttackingPosition = attackingPosition.set("checker",  null);
+            return board.setIn([from.row,from.col], newAttackingPosition);
         }
+    },
+    attackerWins : function(attackingPosition, defendingPosition) {
+        return
+            defendingPosition.get("checker") == null ||
+            attackingPosition.checker.attackRank > defendingPosition.checker.defenceRank ||
+            (attackingPosition.checker.specialAttack && attackingPosition.checker.specialAttack(defendingPosition.checker));
+
     }
 };
