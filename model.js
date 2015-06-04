cc09bc145df619a4ce7a48f9a42ad0c78c32cfa8
Kristoffer Nielsen <kristoffervn@gmail.com>
2015-06-04 10:24:55 +0200

    Simple model

---
+++ model.js
@@ -0,0 +1,12 @@
+function boardPosition() {
+    this.FieldType = ""; // Water, PLain
+    this.checkerType =""; // Type of brik
+
+}
+module.export = {
+    getBoard : boardPosition[10][10],
+    move: function(fromBoardPosition, toBoardPosition())
+    newGame: function()
+    lastMove: function();
+}
+
